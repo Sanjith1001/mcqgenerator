ba07109d263b8ce451659a91f545072122e84175
Sanjith <sanjithpalateru10@gmail.com>
2025-08-21 14:08:22 +0530

    Comment out logger imports and related logging setup in StreamlitAPP.py and logger.py

--- StreamlitAPP.py
+++ StreamlitAPP.py
@@ -8,7 +8,7 @@ load_dotenv()
 import streamlit as st
 from src.mcqgenerator.utils import read_file, get_table_data
 from mcqgenerator.mcqgenerator import generate_evaluate_chain
-from mcqgenerator.logger import logging
+# from mcqgenerator.logger import logging
 
 # Load response schema (if needed)
 with open("response.json", "r") as f:
